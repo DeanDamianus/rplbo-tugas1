70fd49201c959d600904b7a50c939e8e730fc739
DeanDamianus <damianus.atisanta@ti.ukdw.ac.id>
2022-02-08 21:33:35 +0700

    Testing Baru
    bikin template java + belajar

---
+++ Group D/71200569.java
@@ -0,0 +1,21 @@
+	class Hitung71200569 {
+		public static void main(String args[]) {
+			int x=10;
+			int y=25;
+			int z=x+y;
+
+			System.out.println("Hasil penjumlahan"
+				+" dari x+y = " + z);
+			System.out.println("Hasil akar kuadrato "
+				+"dari z = " + Math.sqrt(z));
+		}
+	}
+
+	class Nama71200569 {
+		public static void main(String args[]) {
+			String namadepan = "Damianus";
+			System.out.println("Nama depan saya adalah = " + namadepan);
+			System.out.println("Nama lengkap saya "
+				+"adalah = " + namadepan + "Atisantao");
+		}
+	}
